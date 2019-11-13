4857f74fa40ea67a7514d865d8bc055e3249eab4
Nixon Duong <niduong@ucsc.edu>
2019-11-12 23:54:00 -0800

    Hello World flask server

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
-from flask import Flask, escape, request
+from flask import Flask
+from keras.models import Sequential
 
 app = Flask(__name__)
 
-@app.route('/')
-def prediction():
+def loadModel():
     json_file = open('model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     loaded_model.load_weights('./modelBin/model.h5')
     loaded_model.compile(close='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-    
-    return f'Model has been compiled'
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
